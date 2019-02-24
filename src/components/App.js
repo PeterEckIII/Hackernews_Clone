e56573b74b9197eca5e4fe5cbbd78e41d16dccdf
Peter Eck <jpeckiii@gmail.com>
2019-02-24 11:29:42 -0600

    Restructure app

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "../styles/App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <li>Hi!</li>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
