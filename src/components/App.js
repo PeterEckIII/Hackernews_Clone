91ba61f871dd4d777f192fb54e2a52ade0b45507
Peter Eck <jpeckiii@gmail.com>
2019-02-24 12:24:50 -0600

    Add CreateLink

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,10 @@
 import React, { Component } from "react";
 import "../styles/App.css";
+import CreateLink from "./CreateLink";
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <div>
-          <li>Hi!</li>
-        </div>
-      </div>
-    );
+    return <CreateLink />;
   }
 }
 
