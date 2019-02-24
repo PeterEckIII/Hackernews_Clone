269ed49ff814f93d1555e165398ee13617a53d3a
Peter Eck <jpeckiii@gmail.com>
2019-02-24 12:43:27 -0600

    Put together homepage of app

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,23 @@
 import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
 import "../styles/App.css";
 import CreateLink from "./CreateLink";
+import LinkList from "./LinkList";
+import Header from "./Header";
 
 class App extends Component {
   render() {
-    return <CreateLink />;
+    return (
+      <div className="center w85">
+        <Header />
+        <div className="ph3 pv1 background-grey">
+          <Switch>
+            <Route exact path="/" component={LinkList} />
+            <Route exact path="/create" component={CreateLink} />
+          </Switch>
+        </div>
+      </div>
+    );
   }
 }
 
