029440cd9d78ec970b44ef6dfd8cd22a0e07f5cf
Peter Eck <jpeckiii@gmail.com>
2019-02-25 13:00:08 -0600

    Convert to functional component

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,15 +1,13 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Link extends Component {
-  render() {
-    return (
-      <>
-        <div>
-          {this.props.link.description} ({this.props.link.url})
-        </div>
-      </>
-    );
-  }
-}
+const Link = props => {
+  return (
+    <>
+      <div>
+        {props.link.description} ({props.link.url})
+      </div>
+    </>
+  );
+};
 
 export default Link;
