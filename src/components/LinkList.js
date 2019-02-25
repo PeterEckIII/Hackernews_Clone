ef612dde9f56d7884df7767379142d46ee0c1930
Peter Eck <jpeckiii@gmail.com>
2019-02-25 13:00:28 -0600

    Convert to functional component

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import Link from "./Link";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
@@ -16,26 +16,24 @@ const FEED_QUERY = gql`
   }
 `;
 
-class LinkList extends Component {
-  render() {
-    return (
-      <Query query={FEED_QUERY}>
-        {({ loading, error, data }) => {
-          if (loading) return <div>Fetching...</div>;
-          if (error) return <div>Error</div>;
-          const linksToRender = data.feed.links;
+const LinkList = props => {
+  return (
+    <Query query={FEED_QUERY}>
+      {({ loading, error, data }) => {
+        if (loading) return <div>Fetching...</div>;
+        if (error) return <div>Error</div>;
+        const linksToRender = data.feed.links;
 
-          return (
-            <div>
-              {linksToRender.map(link => (
-                <Link key={link.id} link={link} />
-              ))}
-            </div>
-          );
-        }}
-      </Query>
-    );
-  }
-}
+        return (
+          <div>
+            {linksToRender.map(link => (
+              <Link key={link.id} link={link} />
+            ))}
+          </div>
+        );
+      }}
+    </Query>
+  );
+};
 
 export default LinkList;
