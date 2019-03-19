aa9db394c69c1daced253d6bf6013d254ed7e089
Peter Eck <petereck@Pete-Eck-MacBook-Pro.local>
2019-03-19 16:15:54 -0500

    Implement subscription and search features

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,17 +1,42 @@
 import React from "react";
-import { className } from "postcss-selector-parser";
+// import { className } from "postcss-selector-parser";
+import { Mutation } from "react-apollo";
+import gql from "graphql-tag";
 import { AUTH_TOKEN as authToken } from "../constants";
 import timeDifferenceForDate from "../utils";
+
+const VOTE_MUTATION = gql`
+  mutation VoteMutation($linkId: ID!) {
+    vote(linkId: $linkId) {
+      id
+      link {
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
+    }
+  }
+`;
 
 const Link = props => {
   return (
     <div className="flex mt2 items-start">
       <div className="flex items-center">
-        <span className="gray"> {props.index + 1} </span>
+        <span className="gray">{props.index + 1}.</span>
         {authToken && (
-          <div className="ml1 gray f11" onClick={() => props._voteForLink()}>
-            ▲
-          </div>
+          <Mutation mutation={VOTE_MUTATION} variables={{ linkId: props.link.id }} update={(store, {data: {vote} }) => props.updateStoreAfterVote(store, vote, props.link.id)}>
+            {voteMutation => (
+              <div className="ml1 gray f11" onClick={voteMutation}>
+                ▲
+              </div>
+            )}
+          </Mutation>
         )}
       </div>
       <div className="ml1">
@@ -20,8 +45,7 @@ const Link = props => {
         </div>
         <div className="f6 lh-copy gray">
           {props.link.votes.length} votes | by{" "}
-          {props.link.postedBy ? props.link.postedBy.name : "Unknown "}
-          {""}
+          {props.link.postedBy ? props.link.postedBy.name : "Unknown "}{""}
           {timeDifferenceForDate(props.link.createdAt)}
         </div>
       </div>
